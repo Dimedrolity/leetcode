5f31499d21bc470ef53fd333833c043f3308c12d
dmitry <dima.redkin.1999@gmail.com>
2024-03-17 18:43:04 +0500

    ref

--- task_53/solution.go
+++ task_53/solution.go
@@ -10,16 +10,13 @@ package task_53
 // Идея в том, чтобы добавлять элемент в сумму, но если этот элемент больше чем (сумма предыдущих + этот элемент),
 // то он становится большей суммой.
 func maxSubArray(nums []int) int {
-	if len(nums) == 1 {
-		return nums[0]
-	}
-
 	localMaxSum := nums[0] // не 0, так как возможны отрицательные числа.
 	globalMaxSum := localMaxSum
-	for i := 1; i < len(nums); i++ {
-		localMaxSum += nums[i]
-		if nums[i] > localMaxSum {
-			localMaxSum = nums[i]
+
+	for _, num := range nums[1:] {
+		localMaxSum += num
+		if num > localMaxSum {
+			localMaxSum = num
 		}
 		globalMaxSum = max(globalMaxSum, localMaxSum)
 	}
@@ -33,12 +30,9 @@ func maxSubArray(nums []int) int {
 // Идея в том, чтобы добавлять элемент в сумму, но если текущая сумма меньше нуля, то обнулять сумму.
 // Имхо, решение чуть более сложное для понимания.
 func maxSubArray_NotMy(nums []int) int {
-	if len(nums) == 1 {
-		return nums[0]
-	}
-
 	globalMaxSum := nums[0] // не 0, так как возможны отрицательные числа.
 	var localMaxSum int
+
 	for _, num := range nums {
 		if localMaxSum < 0 {
 			localMaxSum = 0 // если 2 отриц числа, то нет смысла их складывать, нужно взять наибольшее из них.
